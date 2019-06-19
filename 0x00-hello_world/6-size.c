5be924f0f4ee97dde0f78511a7573b89826a8b8f
Anthony Huggins <huggins9000211@gmail.com>
2019-06-19 14:31:34 -0400

    test

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
+/**
+ * main - Prints a string
+ *
+ *Return: 0
+ */
 int main()
 {
-  printf("Size of a char: %d",sizeof(char));
-  puts(" byte(s)");
-  printf("Size of a int: %d",sizeof(int));
-  puts(" byte(s)");
-  printf("Size of a long int: %d",sizeof(long));
-  puts(" byte(s)");
-  printf("Size of a long long int: %d",sizeof(long long));
-  puts(" byte(s)");
-  printf("Size of a float: %d",sizeof(float));
-  puts(" byte(s)");
-  return 0;
+	printf("Size of a char: %d",sizeof(char));
+	puts(" byte(s)");
+	printf("Size of a int: %d",sizeof(int));
+	puts(" byte(s)");
+	printf("Size of a long int: %d",sizeof(long));
+	puts(" byte(s)");
+	printf("Size of a long long int: %d",sizeof(long long));
+	puts(" byte(s)");
+	printf("Size of a float: %d",sizeof(float));
+	puts(" byte(s)");
+	return 0;
 }
