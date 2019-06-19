4407706a6689d56ddaf84037ebdae64892946b7e
Anthony Huggins <huggins9000211@gmail.com>
2019-06-19 14:30:30 -0400

    test

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main()
+/**
+ * main - Prints a string
+ *
+ *Return: 0
+ */
+int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return 0;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
