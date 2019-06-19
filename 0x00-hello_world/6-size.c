86d146487202371013fa850c4de8dbd5a6532786
Anthony Huggins <huggins9000211@gmail.com>
2019-06-19 12:17:16 -0400

    test

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+  printf("Size of a char: %d",sizeof(char));
+  puts(" byte(s)");
+  printf("Size of a int: %d",sizeof(int));
+  puts(" byte(s)");
+  printf("Size of a long int: %d",sizeof(long));
+  puts(" byte(s)");
+  printf("Size of a long long int: %d",sizeof(long long));
+  puts(" byte(s)");
+  printf("Size of a float: %d",sizeof(float));
+  puts(" byte(s)");
+  return 0;
+}
