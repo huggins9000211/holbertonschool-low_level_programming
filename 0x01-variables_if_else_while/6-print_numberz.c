a24d308339468ac5a0c424dc4fca6c89cd232015
Anthony Huggins <huggins9000211@gmail.com>
2019-06-20 15:13:57 -0400

    test

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,14 +8,13 @@
  */
 int main(void)
 {
-	char abc[10] = "0123456789";
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = "0"; i < 10; i++)
 	{
-		putchar(abc[i]);
+		putchar(i);
 	}
 
-	putchar('\n');
+	putchar(\n);
 	return (0);
 }
