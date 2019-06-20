9508c8d2741f42ffee83ce51d97438e37ff9d705
Anthony Huggins <huggins9000211@gmail.com>
2019-06-20 16:25:28 -0400

    test

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,13 +12,13 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
+		putchar(i);
 		if (i == '9')
 		{
-			putchar(i);
+				
 		}
 		else
 		{
-			putchar(i);
 			putchar(',');
 			putchar(' ');
 		}
