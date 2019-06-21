c5f9e72f60462dd2f083eef5098dda5a7ed30a1d
Anthony Huggins <huggins9000211@gmail.com>
2019-06-21 14:33:25 -0400

    test

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+/* prints the alphabe */
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints a string
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int i;
+	int c;
+	int a;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (c = i + 1; c <= '9'; c++)
+		{
+			for (a = c + 1; a < '9'; a++)
+			{
+				if (a != '2')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				putchar(i);
+				putchar(c);
+				putchar(a);
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
