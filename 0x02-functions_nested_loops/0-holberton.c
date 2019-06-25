6801157c8f7d324ef9ee8bad8e1690240c00ac06
Anthony Huggins <huggins9000211@gmail.com>
2019-06-25 16:03:18 -0400

    test

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,13 @@
+/* prints the alphabe */
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints a string
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	printf("Holberton\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+/* prints the alphabe */
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints a string
+ *
+ *Return: 0
+ */
+void print_alphabet()
+{
+	printf("abcdefghijklmnopqrstuvwxyz\n");
+	return;
+}
+/**
+ * main - Prints a string
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
