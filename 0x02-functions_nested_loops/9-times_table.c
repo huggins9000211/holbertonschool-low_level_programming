a31783ef9720faee95f6f1076730ec85d1432404
Anthony Huggins <huggins9000211@gmail.com>
2019-06-26 17:32:18 -0400

    test

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,26 +1,99 @@
 /* prints the alphabe */
 #include "holberton.h"
 /**
- * jack_bauer - Prints a string
+ * times_table - Prints a string
  *
- * Return: int
+ * Return: void
  */
 void times_table(void)
 {
 	int a;
 	int b;
 	int c;
-	char d;
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b <= 9; b++) {
-			c = a * b;
-			d = c;
-			_putchar(d);
-			_putchar(',');
-			_putchar(' ');
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		for (b = 1; b <= 9; b++)
+		{
+			if ((a * (b + 1) / 10) >= 1)
+			{
+				if (b == 9)
+				{
+					if ((a * b / 10) >= 1)
+					{
+						c = a * b;
+						_putchar(c / 10 + '0');
+						_putchar(c % 10 + '0');
+					}
+					else
+					{
+						c = a * b;
+						_putchar(c + '0');
+					}
 
+				}
+				else
+				{
+					if ((a * b / 10) >= 1)
+					{
+						c = a * b;
+						_putchar(c / 10 + '0');
+						_putchar(c % 10 + '0');
+						_putchar(',');
+						_putchar(' ');
+					}
+					else
+					{
+						c = a * b;
+						_putchar(c + '0');
+						_putchar(',');
+						_putchar(' ');
+					}
+				}
+
+			}
+			else
+			{
+
+				if (b == 9)
+				{
+					if ((a * b / 10) >= 1)
+					{
+						c = a * b;
+						_putchar(c / 10 + '0');
+						_putchar(c % 10 + '0');
+					}
+					else
+					{
+						c = a * b;
+						_putchar(c + '0');
+					}
+				}
+				else
+				{
+					if ((a * b / 10) >= 1)
+					{
+						c = a * b;
+						_putchar(c / 10 + '0');
+						_putchar(c % 10 + '0');
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					else
+					{
+						c = a * b;
+						_putchar(c + '0');
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+					}
+				}
+			}
 		}
 		_putchar('\n');
 	}
