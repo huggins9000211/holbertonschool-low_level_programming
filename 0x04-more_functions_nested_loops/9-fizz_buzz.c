523732af3699aff2962995112c6acd43336966ea
Anthony Huggins <huggins9000211@gmail.com>
2019-06-27 16:02:31 -0400

    test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,26 +9,52 @@
 int main(void)
 {
 	int i;
-	int c;
 
-	for  i = 1; i <= 100; i++)
+	for  (i = 1; i <= 100; i++)
 	{
-		if (((i % 3) == 0) && ((i % 5) == 0))
+		if i != 100
 		{
-			printf("FizzBuzz");
-		}
-		else if ((i % 3) == 0)
-		{
-			printf("Fizz");
-		}
-		else if ((i % 5) == 0)
-		{
-			printf("Buzz");
+			if (((i % 3) == 0) && ((i % 5) == 0))
+			{
+				printf("FizzBuzz");
+				printf(" ");
+			}
+			else if ((i % 3) == 0)
+			{
+				printf("Fizz");
+				printf(" ");
+			}
+			else if ((i % 5) == 0)
+			{
+				printf("Buzz");
+				printf(" ");
+			}
+			else
+			{
+				printf("%d",i);
+				printf(" ");
+			}
 		}
 		else
 		{
-			printf("%d",i);
+			if (((i % 3) == 0) && ((i % 5) == 0))
+			{
+				printf("FizzBuzz");
+			}
+			else if ((i % 3) == 0)
+			{
+				printf("Fizz");
+			}
+			else if ((i % 5) == 0)
+			{
+				printf("Buzz");
+			}
+			else
+			{
+				printf("%d",i);
+			}
 		}
 	}
 	putchar('\n');
+	return (0);
 }
