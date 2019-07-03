4774a79cac72a368a829feba800fbd0caa28102b
Anthony Huggins <huggins9000211@gmail.com>
2019-07-03 18:23:33 -0400

    test

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,16 +1,47 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * _strlen - Prints a string
+ * @s: char
+ * @i: char
+ * @count: int
+ *
+ * Return: int
+ */
+ int _strlen(char *s)
+ {
+ 	int count;
+
+ 	count = 0;
+ 	while (s[0] != '\0')
+ 	{
+ 		s++;
+ 		count++;
+ 	}
+ 	return (count);
+
+ }
+
+/**
+ * _strcpy - check the code for Holberton School students.
+ * @dest: char
+ * @src: char
+ * @len: int
+ * @count: int
  *
  * Return: Always 0.
  */
 char *_strcpy(char *dest, char *src)
 {
-	len = _len(src);
+	int count;
+	int len;
+
+	count = 0;
+	len = _strlen(src);
 	while (count < len)
 	{
 		dest[count] = src[count];
+		count++;
 	}
 	return dest;
 }
