94664f9aba2f49ee8e4bd00209d1e0b9ebf7969e
Anthony Huggins <huggins9000211@gmail.com>
2019-07-03 16:15:48 -0400

    test

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,6 +2,24 @@
 /**
  * _strlen - Prints a string
  * @s: char
+ *
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (s[0] != '\0')
+	{
+		s++;
+		count++;
+	}
+	return (count);
+}
+/**
+ * _strlen - Prints a string
+ * @s: char
  * @i: char
  * @count: int
  *
@@ -10,17 +28,16 @@
 void print_rev(char *s)
 {
 	int count;
+	int len;
+	char temp;
 
 	count = 0;
-	while (s[count] != '\0')
+	len = _strlen(s)
+	while (s[count] < len)
 	{
-		count++;
-	}
-	count--;
-	while (count >= 0)
-	{
-		count--;
-		_putchar(s[count]);
+		temp = s[count];
+		s[len - count];
+		
 	}
 	_putchar('\n');
 
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	len = _len(src)
+	while (count < len) 
+	{
+		dest[count] = src[count]
+	}
+}
