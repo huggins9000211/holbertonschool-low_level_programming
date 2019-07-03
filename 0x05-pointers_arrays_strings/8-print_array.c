f077a4d4a0c5eadf6036ede507e2c784cf3b4f76
Anthony Huggins <huggins9000211@gmail.com>
2019-07-03 18:47:10 -0400

    test

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+
+/**
+ * _strlen - Prints a string
+ * @s: char
+ * @i: char
+ * @count: int
+ *
+ * Return: int
+ */
+ int _strlen(char *s)
+ {
+ 	int count;
+
+ 	count = 0;
+ 	while (s[0] != '\0')
+ 	{
+ 		s++;
+ 		count++;
+ 	}
+ 	return (count);
+
+ }
+
+/**
+ * _strcpy - check the code for Holberton School students.
+ * @dest: char
+ * @src: char
+ * @len: int
+ * @count: int
+ *
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+	int count;
+	int len;
+
+	count = 0;
+	len = _strlen(src);
+	while (count < n)
+	{
+		_putchar(a[count]);
+		count++;
+	}
+}
