c1ab9d293c3d24843a3f88fead22a478754c590f
Anthony Huggins <huggins9000211@gmail.com>
2019-07-07 14:28:02 -0400

    test

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,44 +1,30 @@
 #include "holberton.h"
-#include <unistd.h>
 /**
  * _strlen - Prints a string
  * @s: char
- * @i: char
- * @count: int
  *
  * Return: int
  */
- int _strlen(char *s)
- {
- 	int count;
+int _strlen(char *s)
+{
+	 int count;
 
- 	count = 0;
- 	while (s[0] != '\0')
- 	{
- 		s++;
- 		count++;
- 	}
- 	return (count);
-
- }
- /**
-  * _putchar - Prints a string
-  * @s: char
-  *
-  * Return: int
-  */
- int _putchar(char c)
- {
- 	return (write(1, &c, 1));
- }
- /**
-  * _strlen - Prints a string
-  * @s: char
-  * @i: char
-  * @count: int
-  *
-  * Return: int
-  */
+	count = 0;
+	while (s[0] != '\0')
+	{
+		s++;
+		count++;
+	}
+	return (count);
+}
+/**
+ * _strncpy - Prints a string
+ * @dest: char
+ * @src: char
+ * @n: int
+ *
+ * Return: int
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int count;
