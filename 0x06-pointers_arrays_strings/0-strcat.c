db6d6d547c667653bd629f0369b241f16c33494a
Anthony Huggins <huggins9000211@gmail.com>
2019-07-07 14:08:11 -0400

    test

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,42 +3,39 @@
 /**
  * _strlen - Prints a string
  * @s: char
- * @i: char
- * @count: int
  *
  * Return: int
  */
- int _strlen(char *s)
- {
- 	int count;
 
- 	count = 0;
- 	while (s[0] != '\0')
- 	{
- 		s++;
- 		count++;
- 	}
- 	return (count);
+int _strlen(char *s)
+{
+	int count;
 
- }
- /**
-  * _putchar - Prints a string
-  * @s: char
-  *
-  * Return: int
-  */
- int _putchar(char c)
- {
- 	return (write(1, &c, 1));
- }
- /**
-  * _strlen - Prints a string
-  * @s: char
-  * @i: char
-  * @count: int
-  *
-  * Return: int
-  */
+	count = 0;
+	while (s[0] != '\0')
+	{
+		s++;
+		count++;
+	}
+	return (count);
+}
+/**
+ * _putchar - Prints a string
+ * @c: char
+ *
+ * Return: int
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+/**
+ * _strcat - Prints a string
+ * @dest: char
+ * @src: char
+ *
+ * Return: int
+ */
 char *_strcat(char *dest, char *src)
 {
 	int i;
@@ -52,7 +49,6 @@ char *_strcat(char *dest, char *src)
 		if (i == len)
 		{
 			dest[i + org] = '\0';
-
 		}
 		else
 		{
