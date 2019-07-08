1ad4d04320de19222f8a1cdb5e5d3d714ae2e062
Anthony Huggins <huggins9000211@gmail.com>
2019-07-08 13:36:55 -0400

    test

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,42 @@
+/**
+ * *cap_string - Prints a string
+ * @s: char
+ *
+ * Return: int
+ */
+char *leet(char *s)
+{
+	int count;
+	int c;
+
+	char *l = "aAeEoOtTlL";
+
+	char *n = "4433007711";
+
+	c = 0;
+	count = 0;
+	while (s[count] != '\0')
+	{
+		if ((s[count] >= 97 && s[count] <= 122) || (s[count] >= 97 && s[count]))
+		{
+			if (count == 0)
+			{
+				s[count] = s[count] - ' ';
+			}
+			else
+			{
+				while (l[c] != '\0')
+				{
+					if (s[count] == l[c])
+					{
+						s[count] = n[c];
+					}
+					c++;
+				}
+				c = 0;
+			}
+		}
+		count++;
+	}
+	return (s);
+}
