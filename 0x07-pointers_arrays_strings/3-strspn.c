3dc46c6b66697e4998b6e67536d403dcf040308d
Anthony Huggins <huggins9000211@gmail.com>
2019-07-09 14:27:53 -0400

    test

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,31 +8,27 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i;
-	char *r;
 	int x;
 	int temp;
-	int check;
-	int extra;
 
 	i = 0;
 	x = 0;
 	temp = 0;
-	check = 0;
 	while (s[i] != '\0')
 	{
-		if (i != extra
-		while (accept[x] != '\0'
+		if (i == temp)
 		{
-			if (s[i] == accept[x]);
+			x = 0;
+			while (accept[x] != '\0')
 			{
-				temp++;
-				check++
-				break
+				if (s[i] == accept[x])
+				{
+					temp++;
+				}
+				x++;
 			}
-			extra = 0;
-			x++;
 		}
-		i++; 
+		i++;
 	}
-	return (check);
+	return (temp);
 }
