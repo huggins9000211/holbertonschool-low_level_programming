484402970012defa1ffa51d38e2e8f432cfe363e
Anthony Huggins <huggins9000211@gmail.com>
2019-07-10 18:31:16 -0400

    test

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,24 +5,6 @@
  *
  * Return: int
  */
-int _strlen(char *s)
-{
-	 int count;
-
-	count = 0;
-	while (s[0] != '\0')
-	{
-		s++;
-		count++;
-	}
-	return (count);
-}
-/**
- * _puts_recursion - check the code for Holberton School students.
- * @s: char
- *
- * Return: Always 0.
- */
 void _puts_recursion(char *s)
 {
 	char *i = s;
@@ -33,7 +15,7 @@ void _puts_recursion(char *s)
 		i = i + 1;
 		_puts_recursion(i);
 	}
-	else if(*i == '\0')
+	else if (*i == '\0')
 	{
 		_putchar('\n');
 	}
