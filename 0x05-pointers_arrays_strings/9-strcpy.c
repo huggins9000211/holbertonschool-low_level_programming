856bf15a70d3869180655011490c0e5bf950b523
Anthony Huggins <huggins9000211@gmail.com>
2019-07-11 14:05:54 -0400

    test

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,25 +3,23 @@
 /**
  * _strlen - Prints a string
  * @s: char
- * @i: char
  * @count: int
  *
  * Return: int
  */
- int _strlen(char *s)
- {
- 	int count;
+int _strlen(char *s)
+{
+	int count;
 
- 	count = 0;
- 	while (s[0] != '\0')
- 	{
- 		s++;
- 		count++;
- 	}
- 	return (count);
+	count = 0;
+	while (s[0] != '\0')
+	{
+		s++;
+		count++;
+	}
+	return (count);
 
- }
-
+}
 /**
  * _strcpy - check the code for Holberton School students.
  * @dest: char
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,54 +1,12 @@
 #include "holberton.h"
 
-int _strlen_recursion(char *s);
-/**
- * _print_rev_recursion - check the code for Holberton School students.
- *
- * @s: char
- * Return: Always 0.
- */
+
 void _print_rev_recursion(char *s)
 {
-	char *i;
-	int count;
-	int len;
-
-	i = s;
-	len = _strlen_recursion(s);
-	count = 0;
-	if (*i != '\0')
+	if (*s == '\0')
 	{
-		count ++;
-		i = i + 1;
-		_print_rev_recursion(i);
+		return;
 	}
-	_putchar(*i);
-	if (len == count)
-	{
-		_putchar('\n');
-	}
-
-
-
-}
-/**
- * _strlen_recursion - check the code for Holberton School students.
- *@s: char
- *
- * Return: Always 0.
- */
-int _strlen_recursion(char *s)
-{
-	int count;
-	char *i;
-
-	i = s;
-	count = 1;
-	if (*i != '\0')
-	{
-		i = i + 1;
-		count = count + _strlen_recursion(i);
-		return (count);
-	}
-	return (0);
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
