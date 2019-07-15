813125b2a599b8b8cf72c04a05329eb35b94e2e6
Anthony Huggins <huggins9000211@gmail.com>
2019-07-14 22:14:31 -0400

    test

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,16 @@
- 
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * main - Prints a string
+ * @argc: char
+ * @argv: char
+ *
+ * Return: int
+ */
+int main(int argc , char *argv[] __attribute__((unused)))
+{
+       printf("%d\n", argc - 1);
+       return (1);
+}
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * main - Prints a string
+ * @argc: char
+ * @argv: char
+ *
+ * Return: int
+ */
+int main(int argc , char *argv[])
+{
+	int count;
+
+	count = 1;
+	while (count < argc)
+	{
+		printf("%s\n", argv[count]);
+		count ++;
+	}
+	return (1);
+}
