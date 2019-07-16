d1db00602f4a22d2eb2e4de8755718a097a1e90f
Anthony Huggins <huggins9000211@gmail.com>
2019-07-16 15:57:39 -0400

    test

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints a string
+ * @argc: char
+ * @argv: char
+ *
+ * Return: int
+ */
+ char *create_array(unsigned int size, char c)
+ {
+	char *result;
+
+	if (size == 0)
+	{
+		return ('\0');
+	}
+	result = (char*)malloc(size * Sizeof(int));
+	*result = c;
+ }
