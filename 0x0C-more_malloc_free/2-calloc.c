ecb3783d2c5867bbe259584344ad0fa69c54136b
Anthony Huggins <huggins9000211@gmail.com>
2019-07-18 23:11:36 -0400

    test

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,11 +11,20 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *result;
+	int *temp;
+	unsigned int i;
 
-	result = calloc(nmemb, size);
+	i = 0;
+	result = malloc(nmemb * size);
+	temp = malloc(nmemb * size);
 	if (result == NULL)
 	{
 		return (NULL);
 	}
+	for (i = 0; i < size; i++)
+	{
+		temp[i] = 0;
+	}
+	result = temp;
 	return (result);
 }
