64eb5ec365dd680080972fd234f95110eb885dda
Anthony Huggins <huggins9000211@gmail.com>
2019-07-22 16:41:50 -0400

    test

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef GRANDPARENT_H
+#define GRANDPARENT_H
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+#endif
