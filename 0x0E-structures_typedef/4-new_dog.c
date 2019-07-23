033cb58b91478c0be3136982bcf53f1efaff1df6
Anthony Huggins <huggins9000211@gmail.com>
2019-07-23 11:10:10 -0400

    test

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -52,16 +52,30 @@ char *_strcpy(char *dest, char *src)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *end_dog;
+	char *n;
+	char *o;
 
 	end_dog = malloc(sizeof(dog_t));
 	if (end_dog == NULL)
 	{
 		return (NULL);
 	}
-
-	end_dog->name = name;
+	n = malloc(sizeof(name));
+	if (!n)
+	{
+		free(end_dog);
+		return (NULL);
+	}
+	o = malloc(sizeof(owner));
+	if (!o)
+	{
+		free(n);
+		free(end_dog);
+		return (NULL);
+	}
+	end_dog->name = n;
 	end_dog->age = age;
-	end_dog->owner = owner;
+	end_dog->owner = o;
 
 
 	return (end_dog);
