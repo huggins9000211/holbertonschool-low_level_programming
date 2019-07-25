c2609a34eac54542c7e4bc73aece4619f83c2aa6
Anthony Huggins <huggins9000211@gmail.com>
2019-07-24 20:55:48 -0400

    test

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -27,13 +27,13 @@ int main(int argc, char *argv[])
 	else if ((*sign == '/' || *sign == '%') && (num2 == 0))
 	{
 		printf("Error\n");
-		return (98);
+		return (100);
 	}
 	f = get_op_func(sign);
 	if (f == NULL)
 	{
 		printf("Error\n");
-		return (98);
+		return (99);
 	}
 	result = (f(num1, num2));
 	printf("%d\n", result);
