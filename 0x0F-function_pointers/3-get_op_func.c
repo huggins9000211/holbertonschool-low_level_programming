81562fc52292cf5e27b65e8ad3f34607c4c51b2d
Anthony Huggins <huggins9000211@gmail.com>
2019-07-24 20:53:11 -0400

    test

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,15 +1,21 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * get_op_func - Struct op
+ *
+ * @s: The function associated
+ * Return: Nothing.
+ */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        	{"+", op_add},
-        	{"-", op_sub},
-        	{"*", op_mul},
-        	{"/", op_div},
-        	{"%", op_mod},
-        	{NULL, NULL}
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
 	int i;
 
@@ -20,8 +26,7 @@ int (*get_op_func(char *s))(int, int)
 		{
 			return (ops[i].f);
 		}
-		i ++;
+		i++;
 	}
-	printf("fail");
 	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,9 +1,15 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - Struct op
+ *
+ * @argc: The function associated
+ * @argv: The function associated
+ * Return: Nothing.
+ */
 int main(int argc, char *argv[])
 {
-	/*char sign;*/
 	int num1;
 	int num2;
 	int result;
@@ -13,19 +19,23 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	sign = (argv[2]);
-	/*printf("%d\n", num1);
-	printf("%d\n", argc);*/
 	if (argc != 4)
 	{
 		printf("Error\n");
 		return (98);
 	}
-	/*else if (sign == ('+' || '-' || '*' || '/' || '%'))
+	else if ((*sign == '/' || *sign == '%') && (num2 == 0))
 	{
-
-	}*/
+		printf("Error\n");
+		return (98);
+	}
 	f = get_op_func(sign);
+	if (f == NULL)
+	{
+		printf("Error\n");
+		return (98);
+	}
 	result = (f(num1, num2));
 	printf("%d\n", result);
-	return 0;
+	return (0);
 }
