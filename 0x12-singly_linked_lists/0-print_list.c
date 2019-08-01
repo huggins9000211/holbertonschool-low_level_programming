5d0d95a369a5d2020c82a377883fe6eb29bbbf11
Anthony Huggins <huggins9000211@gmail.com>
2019-08-01 15:12:37 -0400

    test

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+
+size_t print_list(const list_t *h)
+{
+	char *string;
+	char *len;
+
+	int count;
+
+	if (h != NULL)
+	{
+
+		printf("[%d] %s\n", h->len, h->str);
+		count = print_list(h->next);
+		return (count++);
+
+	}
+	else
+	{
+		return (0);
+	}
+}
