a87a86022c3fe4a3a6c2a2ff202373a240395179
Anthony Huggins <huggins9000211@gmail.com>
2019-08-01 17:52:46 -0400

    test

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,15 +1,8 @@
 void free_list(list_t *head);
 {
-	size_t count;
-
-
-
-
-
 	if (h != NULL)
 	{
-
-		count = list_len(h->next);
+		free_list(h->next);
 		free(h->next);
 		free(h->len);
 		free(h->str);
