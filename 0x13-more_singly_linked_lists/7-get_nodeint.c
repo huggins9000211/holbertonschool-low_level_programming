450dcfbdec5cfd9c8fa2896439e7ffbef9f67d4d
Anthony Huggins <huggins9000211@gmail.com>
2019-08-06 17:37:59 -0400

    test

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	}
 	while (i < index)
 	{
-		if (head->next == NULL && (i == (index -1)))
+		if (head->next == NULL)
 		{
 			return (NULL);
 		}
