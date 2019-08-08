99d272641c87408d80edebd1785fc107b1d6e4e6
Anthony Huggins <huggins9000211@gmail.com>
2019-08-08 16:31:40 -0400

    test

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,10 +1,10 @@
 #ifndef GRANDPARENT_H
 #define GRANDPARENT_H
 
-int putstring(char *in);
-int _strlen(char *s);
+
 int _putchar(char c);
-int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+
 
 #endif
