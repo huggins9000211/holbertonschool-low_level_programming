1f01ae1d9098633c3927610dc12fa5c4fefaadff
Anthony Huggins <huggins9000211@gmail.com>
2019-08-08 13:39:25 -0400

    test

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -11,34 +11,3 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-/**
-* _strlen -  a function that returns the length of a string
-* @s: pointer to the length
-* Return: 0
-*/
-int _strlen(char *s)
-{
-	int length = 0;
-
-	while (s[length] != '\0')
-
-	{
-		length++;
-	}
-
-	return (length);
-}
-/**
-* putstring -  a function that returns the length of a string
-* @in: pointer to the length
-* Return: 0
-*/
-int putstring(char *in)
-{
-	int i;
-	int result;
-	i = _strlen(in);
-
-	result = write(1, in, i);
-	return result;
-}
