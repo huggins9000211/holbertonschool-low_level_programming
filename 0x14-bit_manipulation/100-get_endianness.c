48fb17c9d9fc774789155c4963c2afd834e8ad03
Anthony Huggins <huggins9000211@gmail.com>
2019-08-08 18:17:35 -0400

    test

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,9 +1,16 @@
 #include "holberton.h"
-
+/**
+ * get_endianness - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int get_endianness(void)
 {
 	unsigned int i = 1;
-	char *c = (char*)&i;
+
+	char *c = (char *)&i;
+
 	if (*c)
 		return (1);
 	else
