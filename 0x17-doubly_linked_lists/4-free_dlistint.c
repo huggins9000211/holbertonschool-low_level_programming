f8a2888cd50c416bf48162c11e76fdfb84075022
Anthony <huggins9000211@gmail.com>
2019-12-14 12:55:31 -0500

    test

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -2,31 +2,11 @@
 void free_dlistint(dlistint_t *head)
 {
     dlistint_t *last;
-    if(head->prev != NULL)
+    while (head != NULL)
     {
-
-        while (head->prev != NULL)
-        {
-            head = head->prev;
-        }
-        while (head != NULL)
-        {
-            last = head;
-            head = head->next;
-            free(last);
-        } 
+        last = head;
+        head = last->next;
+        free(last);
     }
-    else if (head->next != NULL)
-    {
-        while (head != NULL)
-        {
-            last = head;
-            head = head->next;
-            free(last);
-        } 
-    }
-    else if (head)
-    {
-        free(head);
-    }
+    
 }
