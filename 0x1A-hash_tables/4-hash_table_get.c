ac2fe0819f24dce7ea06698b10646e90b3242147
Anthony <huggins9000211@gmail.com>
2020-01-19 12:51:54 -0500

    test

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,42 +1,27 @@
 #include "hash_tables.h"
 /**
- * key_index - Hash table data structure
+ * hash_table_get - Hash table data structure
  *
  * @key: The size of the array
  * Each cell of this array is a pointer to the first node of a linked list,
  * because we want our HashTable to use a Chaining collision handling
  *
- * @value: size
- *
  * @ht: size
  *
  * Return: hashTable
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index;
-	hash_node_t *new;
-	char *valueCopy;
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *location = ht->array[index];
 
-	valueCopy = malloc(sizeof(value));
-	if (valueCopy == NULL)
+	while (location)
 	{
-		return (0);
+		if (location->key == key)
+		{
+			return (location->value);
+		}
+		location = location->next;
 	}
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-	{
-		return (0);
-	}
-	if (key[0] == '\0')
-	{
-		return (0);
-	}
-	strcpy(valueCopy, value);
-	index = key_index((const unsigned char *)key, ht->size);
-	new->key = key;
-	new->value = valueCopy;
-	new->next = ht->array[index];
-	ht->array[index] = new;
-	return (1);
+	return (NULL);
 }
