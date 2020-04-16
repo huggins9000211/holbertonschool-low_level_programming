70ebfef67d2418be5218e2c3fce57c32eec1a51a
Anthony <huggins9000211@gmail.com>
2020-04-16 15:19:55 -0400

    test

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,20 +1,28 @@
 #include "search.h"
 #include <stdio.h>
 
+/**
+ * linear_search - check the code for Holberton School students.
+ *
+ * @array: test
+ * @size: test
+ * @value: test
+ * Return: Always 0.
+ */
 int linear_search(int *array, size_t size, int value)
 {
-    int i = 0;
-    int sizee = size;
+	int i = 0;
+	int sizee = size;
 
-    if (array == 0)
-        return (-1);
+	if (array == 0)
+		return (-1);
 
-    while (i < sizee)
-    {
-        printf("Value checked array[%d] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (i);
-        i++;
-    }
-    return (-1);
+	while (i < sizee)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,5 @@
 #ifndef GRANDPARENT_H
 #define GRANDPARENT_H
-#include <stdio.h>
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
