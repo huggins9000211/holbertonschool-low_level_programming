91570855475baf53638a77a4d38e0eadb0b9339e
Anthony <huggins9000211@gmail.com>
2020-04-16 15:47:46 -0400

    test

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,48 @@
+#include "search.h"
+#include <stdio.h>
+
+/**
+ * binary_search - check the code for Holberton School students.
+ *
+ * @array: test
+ * @size: test
+ * @value: test
+ * Return: Always 0.
+ */
+void printArr(int *array, int i1, int i2)
+{
+    if (i1 <= i2)
+        printf("Searching in array: ");
+    while (i1 <= i2)
+    {
+        printf("%d", array[i1]);
+        if (i1 != i2)
+            printf(",");
+        i1++;
+    }
+    printf("\n");
+}
+int binarySearchH(int arr[], int l, int r, int x) 
+{
+    printArr(arr, l, r);
+    if (r >= l) { 
+        int mid = l + (r - l) / 2; 
+  
+        if (arr[mid] == x) 
+            return (mid); 
+  
+        if (arr[mid] > x) 
+            return (binarySearchH(arr, l, mid - 1, x)); 
+  
+        return (binarySearchH(arr, mid + 1, r, x)); 
+    } 
+  
+    return (-1); 
+}
+int binary_search(int *array, size_t size, int value)
+{
+    if (array == 0)
+		return (-1);
+
+	return binarySearchH(array, 0, size - 1, value);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,5 +3,7 @@
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int binarySearchH(int arr[], int l, int r, int x);
 
 #endif
