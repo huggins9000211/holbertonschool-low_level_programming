11019f9e8a99c38fb182faa131a5b876b902ea82
Anthony <huggins9000211@gmail.com>
2020-04-16 15:52:28 -0400

    test

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -2,6 +2,54 @@
 #include <stdio.h>
 
 /**
+ * printArr - check the code for Holberton School students.
+ *
+ * @array: test
+ * @i1: test
+ * @i2: test
+ * Return: Always 0.
+ */
+void printArr(int *array, int i1, int i2)
+{
+	if (i1 <= i2)
+		printf("Searching in array: ");
+	while (i1 <= i2)
+	{
+		printf("%d", array[i1]);
+		if (i1 != i2)
+			printf(", ");
+		i1++;
+	}
+	printf("\n");
+}
+/**
+ * binarySearchH - check the code for Holberton School students.
+ *
+ * @arr: test
+ * @l: test
+ * @r: test
+ * @x: test
+ * Return: Always 0.
+ */
+int binarySearchH(int arr[], int l, int r, int x)
+{
+	printArr(arr, l, r);
+	if (r >= l)
+	{
+		int mid = l + (r - l) / 2;
+
+		if (arr[mid] == x)
+			return (mid);
+
+		if (arr[mid] > x)
+			return (binarySearchH(arr, l, mid - 1, x));
+
+		return (binarySearchH(arr, mid + 1, r, x));
+	}
+
+	return (-1);
+}
+/**
  * binary_search - check the code for Holberton School students.
  *
  * @array: test
@@ -9,40 +57,10 @@
  * @value: test
  * Return: Always 0.
  */
-void printArr(int *array, int i1, int i2)
-{
-    if (i1 <= i2)
-        printf("Searching in array: ");
-    while (i1 <= i2)
-    {
-        printf("%d", array[i1]);
-        if (i1 != i2)
-            printf(", ");
-        i1++;
-    }
-    printf("\n");
-}
-int binarySearchH(int arr[], int l, int r, int x) 
-{
-    printArr(arr, l, r);
-    if (r >= l) { 
-        int mid = l + (r - l) / 2; 
-  
-        if (arr[mid] == x) 
-            return (mid); 
-  
-        if (arr[mid] > x) 
-            return (binarySearchH(arr, l, mid - 1, x)); 
-  
-        return (binarySearchH(arr, mid + 1, r, x)); 
-    } 
-  
-    return (-1); 
-}
 int binary_search(int *array, size_t size, int value)
 {
-    if (array == 0)
+	if (array == 0)
 		return (-1);
 
-	return binarySearchH(array, 0, size - 1, value);
+	return (binarySearchH(array, 0, size - 1, value));
 }
